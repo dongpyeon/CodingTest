559e04ed9912c93ecde440adbabdcb56b0bd4b23
dongpyeon <kd0948@naver.com>
2024-04-07 23:52:26 +0900

    24_04_07 프로그래머스 정렬 > K번째수
    Arrays.copyOfRange(배열,시작인덱스,끝인덱스도포함);

--- src/programmers_240407.java
+++ src/programmers_240407.java
@@ -1,6 +1,20 @@
+import java.util.*;
 class programmers_240407 {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
+
+        int[] answer = new int[commands.length];
+
+        for(int m=0; m<commands.length; m++)
+        {
+            int i = commands[m][0];
+            int j = commands[m][1];
+            int k = commands[m][2];
+
+            int temp [] = Arrays.copyOfRange(array,i-1,j);
+            Arrays.sort(temp);
+            answer[m]=temp[k-1];
+        }
+
         return answer;
     }
         //코딩테스트 연습 > 정렬 > K번째수
