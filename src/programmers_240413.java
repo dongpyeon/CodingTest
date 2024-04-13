68bb7b7f310365330eeb7cba0a2e8b8dad812dfe
dongpyeon <kd0948@naver.com>
2024-04-13 23:35:18 +0900

    24_04_13 프로그래머스 코딩테스트 연습 > 완전탐색 > 최소직사각형

---
+++ src/programmers_240413.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class programmers_240413 {
+    public int solution(int n, int[] lost, int[] reserve) {
+        List<Integer> lostList = new ArrayList<>();
+        for (int l : lost) {
+            lostList.add(l);
+        }
+
+        List<Integer> reserveList = new ArrayList<>();
+        for (int r : reserve) {
+            reserveList.add(r);
+        }
+
+        Collections.sort(lostList);
+        Collections.sort(reserveList);
+
+        // 여벌 체육복을 가져왔지만 도난당한 학생 처리
+        List<Integer> removeList = new ArrayList<>();
+        for (int lostStudent : lostList) {
+            if (reserveList.contains(lostStudent)) {
+                removeList.add(lostStudent);
+            }
+        }
+
+        lostList.removeAll(removeList);
+        reserveList.removeAll(removeList);
+
+        int answer = n - lostList.size();
+
+        for (int i = 0; i < lostList.size(); i++) {
+            int lostStudent = lostList.get(i);
+            for (int j = 0; j < reserveList.size(); j++) {
+                int reserveStudent = reserveList.get(j);
+                if (lostStudent == reserveStudent || lostStudent == reserveStudent - 1 || lostStudent == reserveStudent + 1) {
+                    reserveList.remove(j);
+                    answer++;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+    //코딩테스트 연습 > 완전탐색 > 최소직사각형
+}
