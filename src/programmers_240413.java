b3c6b23ffdaa9eba6374e61b98514de0ae843a57
dongpyeon <kd0948@naver.com>
2024-04-14 23:31:40 +0900

    24_04_13 프로그래머스 코딩테스트 연습 > 완전탐색 > 최소직사각형

--- src/programmers_240413.java
+++ src/programmers_240413.java
@@ -3,46 +3,30 @@ import java.util.Collections;
 import java.util.List;
 
 class programmers_240413 {
-    public int solution(int n, int[] lost, int[] reserve) {
-        List<Integer> lostList = new ArrayList<>();
-        for (int l : lost) {
-            lostList.add(l);
-        }
+    public int solution(int[][] sizes) {
+        int big, small;
+        int mostsmall =0;
+        int mostbig =0;
+        for(int i=0; i<sizes.length; i++)
+        {
+            big = sizes[i][0];
+            small = sizes[i][1];
 
-        List<Integer> reserveList = new ArrayList<>();
-        for (int r : reserve) {
-            reserveList.add(r);
-        }
-
-        Collections.sort(lostList);
-        Collections.sort(reserveList);
-
-        // 여벌 체육복을 가져왔지만 도난당한 학생 처리
-        List<Integer> removeList = new ArrayList<>();
-        for (int lostStudent : lostList) {
-            if (reserveList.contains(lostStudent)) {
-                removeList.add(lostStudent);
+            if(big<small)
+            {
+                int temp = small;
+                small=big;
+                big=temp;
             }
+
+            if(mostbig<big)
+                mostbig=big;
+
+            if(mostsmall<small)
+                mostsmall=small;
+
         }
-
-        lostList.removeAll(removeList);
-        reserveList.removeAll(removeList);
-
-        int answer = n - lostList.size();
-
-        for (int i = 0; i < lostList.size(); i++) {
-            int lostStudent = lostList.get(i);
-            for (int j = 0; j < reserveList.size(); j++) {
-                int reserveStudent = reserveList.get(j);
-                if (lostStudent == reserveStudent || lostStudent == reserveStudent - 1 || lostStudent == reserveStudent + 1) {
-                    reserveList.remove(j);
-                    answer++;
-                    break;
-                }
-            }
-        }
-
+        int answer = mostbig*mostsmall;
         return answer;
-    }
     //코딩테스트 연습 > 완전탐색 > 최소직사각형
 }
