970503ee7490860cef2aaa320d5ce11135984a38
dongpyeon <kd0948@naver.com>
2024-04-14 23:32:01 +0900

    24_04_13 프로그래머스 코딩테스트 연습 > 완전탐색 > 최소직사각형

--- src/programmers_240413.java
+++ src/programmers_240413.java
@@ -5,28 +5,27 @@ import java.util.List;
 class programmers_240413 {
     public int solution(int[][] sizes) {
         int big, small;
-        int mostsmall =0;
-        int mostbig =0;
-        for(int i=0; i<sizes.length; i++)
-        {
+        int mostsmall = 0;
+        int mostbig = 0;
+        for (int i = 0; i < sizes.length; i++) {
             big = sizes[i][0];
             small = sizes[i][1];
 
-            if(big<small)
-            {
+            if (big < small) {
                 int temp = small;
-                small=big;
-                big=temp;
+                small = big;
+                big = temp;
             }
 
-            if(mostbig<big)
-                mostbig=big;
+            if (mostbig < big)
+                mostbig = big;
 
-            if(mostsmall<small)
-                mostsmall=small;
+            if (mostsmall < small)
+                mostsmall = small;
 
         }
-        int answer = mostbig*mostsmall;
+        int answer = mostbig * mostsmall;
         return answer;
-    //코딩테스트 연습 > 완전탐색 > 최소직사각형
+        //코딩테스트 연습 > 완전탐색 > 최소직사각형
+    }
 }
