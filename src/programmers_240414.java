78b0478ea6afbe42e2683390d9ee9fbd6638ca11
dongpyeon <kd0948@naver.com>
2024-04-14 23:55:26 +0900

    24_04_14 프로그래머스 코딩테스트 연습 > 완전탐색 > 모의고사

---
+++ src/programmers_240414.java
@@ -0,0 +1,16 @@
+class programmers_240414 {
+    public int[] solution(int[] answers) {
+        int[] su1 = {1,2,3,4,5};
+        int[] su2 = {2,1,2,3,2,4,2,5};
+        int[] su3 = {3,3,1,1,2,2,4,4,5,5};
+
+        for(int i =0; i<answers.length; i++)
+        {
+            if(answers[i]==su1[i%su1.length])
+        }
+
+        int[] answer = {};
+        return answer;
+    }
+    //코딩테스트 연습 > 완전탐색 > 모의고사
+}
