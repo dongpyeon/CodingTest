aa74d99bb92289223dbf9e8000a6131516f25c6f
dongpyeon <kd0948@naver.com>
2024-04-15 23:54:06 +0900

    24_04_15 프로그래머스 코딩테스트 연습 > 완전탐색 > 카펫

---
+++ src/programmers_240415.java
@@ -0,0 +1,29 @@
+class programmers_240415 {
+    public int[] solution(int brown, int yellow) {
+
+        int x=0;
+        int y=0;
+        int z=0;
+        int all = brown+yellow;
+        List<Integer> list = new ArrayList<>();
+
+        //일단 약수여야되고
+        //세로 즉 y 제곱이가 옐로보다 커야해
+        //이렇게함해보자
+        while(z!=all)
+        {
+            z++;
+            if(0==(all%z))
+                list.add(z);
+        }
+        int[] answer = [list.size()];
+        for(int a : list)
+            answer[i]=a;
+
+
+
+        return answer;
+
+    }
+    //코딩테스트 연습 > 완전탐색 > 카펫
+}
