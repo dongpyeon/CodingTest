31fa9aef85f4bec76cc3fac71bfd1ff13179ee4e
dongpyeon <kd0948@naver.com>
2024-04-17 23:52:00 +0900

    24_04_15 프로그래머스 코딩테스트 연습 > 연습문제 > 크기가 작은 부분 문자열

---
+++ src/programmers_240417.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class programmers_240417 {
+    public int[] solution(int brown, int yellow) {
+
+        int x=0;
+        int y=0;
+        int z=0;
+        int all = brown+yellow;
+        List<Integer> list = new ArrayList<>();
+
+        //일단 약수여야되고
+        //세로 즉 y 제곱이가 옐로보다 커야해
+        //이렇게함해보자
+        while(z!=all)
+        {
+            z++;
+            if(0==(all%z))
+                list.add(z);
+        }
+        int[] answer = [list.size()];
+        for(int a : list)
+            answer[i]=a;
+
+
+
+        return answer;
+
+    }
+    //코딩테스트 연습 > 연습문제 > 크기가 작은 부분 문자열
+}
