c19c8bd9062b43b4dda52f1978b474140d732841
dongpyeon <kd0948@naver.com>
2024-04-27 22:25:33 +0900

    24_04_27 프로그래머스 코딩테스트 연습 > 코딩테스트 입문 > 옹알이(1) 수정

--- src/programmers_240426.java
+++ src/programmers_240426.java
@@ -1,31 +1,23 @@
 class programmers_240426 {
     public int solution(String[] babbling) {
         int answer = 0;
-
-        String[] canSpeak = { "aya", "ye", "woo", "ma" };
-        String splitStr = null;
+        String[] ongal = {"aya", "ye", "woo", "ma"};
 
         for (int i = 0; i < babbling.length; i++) {
-            splitStr = babbling[i];
-
-            for (int j = 0; j < canSpeak.length; j++) {
-
-                if (splitStr.length() != 0 && splitStr.contains(canSpeak[j])) {
-
-                    if (splitStr.equals(babbling[i])) {
-                        splitStr = babbling[i].replaceAll(canSpeak[j], "0");
-                    } else {
-                        splitStr = splitStr.replaceAll(canSpeak[j], "0");
-                    }
-                }
+            String temp = babbling[i];
+            for (int j = 0; j < ongal.length; j++) {
+                temp = temp.replace(ongal[j], " ");
             }
-
-            if (splitStr.replaceAll("0", "") == "") {
+            temp = temp.replace(" ", "");
+            if (temp.isEmpty())
                 answer++;
-            }
         }
 
         return answer;
+        //발음가능한 옹알이를 배열 빼고
+        // babbling에서 옹알이 부분을 전부 띄어쓰기로 바꿈
+        // 띄어쓰기를 공백으로 바꾸고
+        // 결국 공백이면 발음가능한것
     }
 
 
