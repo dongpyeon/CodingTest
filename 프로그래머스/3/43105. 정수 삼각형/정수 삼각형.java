9359100890da7b9694b3c8a6299f98880b9afe1d
김동현 <143480675+dongpyeon@users.noreply.github.com>
2024-05-15 21:29:09 +0900

    [level 3] Title: 정수 삼각형, Time: 9.80 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,23 +1,25 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] triangle) {
-        int answer = 0;
-        int[][] dp = new int[triangle.length+1][triangle.length];
-        
-        for(int i = 1; i<triangle.length+1; i++){
-            for(int j = 0; j<triangle[i-1].length; j++){
-                for(int k = j-1; k<j+1; k++){
-                                        
-                    if(k < 0) continue;
-                    
-                    dp[i][j] = Math.max(dp[i][j], triangle[i-1][j] + dp[i-1][k]);
-                    
-                    answer = Math.max(dp[i][j], answer);
-                }
-            }
-        }
-        
-        return answer;
+        int n = triangle.length;
+        int answer=0;
+        for(int i = 1; i < n ; i++) {
+	for(int j = 0; j < triangle[i].length ; j++) {
+
+		if(j == 0) {//왼쪽 끝
+			triangle[i][j] += triangle[i-1][j];
+		}
+		else if(j == i) {//오른쪽 끝
+			triangle[i][j] += triangle[i-1][j-1];
+		}
+		else {
+			triangle[i][j] += Math.max(triangle[i-1][j], triangle[i-1][j-1]);
+		}
+
+		answer = Math.max(answer, triangle[i][j]);
+	}
+}
+return answer;
     }
 }
 // 답안참조 출처: https://ansdyd23.tistory.com/80 [일어나 코드짜야지:티스토리]
