1485f0c44b0eb4501ecc5de7ace003aaa309765d
김동현 <143480675+dongpyeon@users.noreply.github.com>
2024-05-30 11:43:17 +0900

    [Bronze I] Title: 세로읽기, Time: 204 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String arr[] = new String[5];
+        for(int i=0; i<5; i++)
+        {
+            arr[i] = sc.nextLine();
+        }
+        
+        for(int i=0; i<15; i++){
+            for(int j=0; j<5; j++){
+                if(i<arr[j].length()){
+                    System.out.print(arr[j].charAt(i));
+            }
+        }
+    }
+        
+    }
+}
