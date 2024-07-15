bae7f45ccf61ef374deb35ff7481b8fe884a689d
김동현 <143480675+dongpyeon@users.noreply.github.com>
2024-07-16 00:07:39 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 4.23 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,19 +1,12 @@
 class Solution {
-    public String solution(String phone_number) {
-        String answer = "";
-        int i =0;
-        for(i=0; i<phone_number.length()-4; i++)
-        {
-            answer+="*";
-        }
-        
-        while(i<phone_number.length())
-        {
-            String n = String.valueOf(phone_number.charAt(i));
-            answer+=n;
-            i++;
-        }
-        
+  public String solution(String phone_number) {
+      String answer = "";
+
+        for (int i = 0; i < phone_number.length() - 4; i++)
+            answer += "*";
+
+        answer += phone_number.substring(phone_number.length() - 4);
+
         return answer;
-    }
+  }
 }
